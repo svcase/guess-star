a0b7ac9f4dd0b35e33d6c1696ee763c4bdcb8bb6
Sam Case <samcase@Sams-MacBook-Pro.local>
2023-05-18 14:27:31 -0500

    ui

--- public/app.js
+++ public/app.js
@@ -1,50 +1,51 @@
 const audio = document.getElementById('player');
 const playPause = document.getElementById('togglePlay');
 
-let second = 00;
-let count = 00;
+audio.addEventListener('timeupdate',function(){
+  let timer = audio.currentTime * 1000;
+  let secs = "";         
+  let mills = "";    
+  if (timer < 1000) {
+    secs = "00";
+  } else if (timer < 10000) {
+    secs = ("0" + timer).substring(0,2);
+  } else { 
+    secs = timer.toString().substring(0,2);
+  }
+
+if (timer < 10000) {
+    mills = ("0" + timer).substring(2,4);
+} else {
+  mills = timer.toString().substring(2,4);
+}
+
+document.getElementById('count').innerHTML = mills;
+document.getElementById('sec').innerHTML = secs;
+
+if (secs == "25") {
+    audio.pause();
+    document.getElementById('count').innerHTML = "00";
+    playPause.disabled = true;
+}
+
+},false);
+
 
 playPause.addEventListener('click', function() {
   if (audio.paused == true) {
     audio.play();
     playPause.innerHTML = '&#10074; &#10074';
-    timer = true;
-    stopWatch();
   } else {
     audio.pause();
     playPause.innerHTML = '&#9658';
-    timer = false;
-    stopWatch();
   }
 });
 
-function stopWatch() {
-  if (timer) {
-      count++;
-
-      if (count == 100) {
-          second++;
-          count = 0;
-      }
-
-      if (second == 20) {
-          timer = false;
-          playPause.disabled = true;
-      }
-
-      let secString = second;
-      let countString = count;
-
-      if (second < 10) {
-          secString = "0" + secString;
-      }
-
-      if (count < 10) {
-          countString = "0" + countString;
-      }
-
-      document.getElementById('sec').innerHTML = secString;
-      document.getElementById('count').innerHTML = countString;
-      setTimeout(stopWatch, 10);
-  }
+function wrongGuess() {
+  document.getElementById('songSearch').style.backgroundColor = '#f6a0a0';
+  document.getElementById('thisGuess').style.color = '#f6a0a0';
+  setTimeout(() => { 
+    document.getElementById('songSearch').style.backgroundColor = 'white';
+    document.getElementById('thisGuess').style.color = 'white';
+   }, 250);
 };
