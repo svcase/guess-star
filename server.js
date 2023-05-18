843d3caa60673d936018d58b4afa10b5b4b6b9ee
Sam Case <samcase@Sams-MacBook-Pro.local>
2023-05-17 22:27:17 -0500

    new port

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.get("/", (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('listening on 3000');
-  });
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log(`Example app is listening on port ${port}.`)
+);
